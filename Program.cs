bedfb67227b53752c2248d6563527546b998f04e
Chen Guo (RIT Student) <cg2531@ad.rit.edu>
2019-09-04 18:32:10 -0400

    Upload ICE 03

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ICE_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 1999;
+            string c = "Jiangsu Rudong Senior High School";
+            int d = 2018;
+            string e = "Hongmei";
+            string f = "Zhiyong";
+            int g = 4;
+            string h = "NMAD 180 Programming Fund I: Mobile Domain";
+            string i = "Chen";
+            string j = "Guo";
+            Console.WriteLine("My name is " + i + " " + j + "." + "I'm " + a + " years old.");
+            Console.WriteLine("I went to " + c + " for high school and graduated in " + d + ". After that, I decided to come to NTID for college. Both my Mom " +  e + " and Dad " + f + " thought NTID would be a great choice for me.");
+
+            Console.WriteLine("So, now I'm a student here at RIT/NTID. I'm in the " + h + " program, taking " + g + " credits. That's pretty crazy for someone who was born in " + b + ".");
+        }
+    }
+}
